a20bd1890d219ffb529ade65d8100984d8b7b680
Eben <ebensenai2019@gmail.com>
2025-02-19 20:39:50 +0530

    empty square

--- src/script.jsx
+++ src/script.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Square from './Square.jsx'
+import Board from './Square.jsx'
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Square />
+    <Board />
   </>
 )
 
