dbcd7970e8e461b33d246dcc799329d9a061b6ca
Eben <ebensenai2019@gmail.com>
2025-02-19 20:45:16 +0530

    handle Clicks

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,12 @@
 
 function Square({ value }) {
+
+  function handleClick() {
+    console.log('Clicked')
+  }
+
   return <>
-    <button className="square">{value}</button>
+    <button className="square" onClick={handleClick}>{value}</button>
   </>
 }
 
@@ -9,19 +14,19 @@ function Square({ value }) {
 export default function Board() {
   return <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={1} />
+      <Square value={2} />
+      <Square value={3} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={4} />
+      <Square value={5} />
+      <Square value={6} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={7} />
+      <Square value={8} />
+      <Square value={9} />
     </div>
   </>
 
