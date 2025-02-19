6115160db3a32469d9cb2ce9249c8f8f5c94a764
Eben <ebensenai2019@gmail.com>
2025-02-19 20:28:49 +0530

    init

---
+++ src/script.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Square from './Square.jsx'
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <Square />
+  </>
+)
+
